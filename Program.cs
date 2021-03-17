a046f12487605e80d622867447091194de7f213d
Uwe Baumann <uwe.baumann@hotmail.de>
2021-03-17 08:41:04 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace actiontest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
